38657c4a8209691592b4777ce05121ac94f08b5b
ziyuan <hanziyuan08@outlook.com>
2020-04-11 01:05:51 +0800

    获取磁力并添加到 qBittorrent 中

--- cmd/rss-torrent/rss-torrent.go
+++ cmd/rss-torrent/rss-torrent.go
@@ -1,12 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"github.com/go-resty/resty/v2"
 	"github.com/mmcdole/gofeed"
+	"log"
+	"net/http/cookiejar"
+	"strings"
 )
 
 func main() {
+	rssUrl := flag.String("rss", "", "rss url")
+	qBittorrentApiUrl := flag.String("qbittorrent", "", "qbittorrent api url")
+	qBittorrentUsername := flag.String("qbittorrent-username", "", "qbittorrent username")
+	qBittorrentPassword := flag.String("qbittorrent-password", "", "qbittorrent password")
+	flag.Parse()
+
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("https://rsshub-bva.herokuapp.com/eztv/torrents/6048596")
-	fmt.Println(feed)
+	feed, err := fp.ParseURL(*rssUrl)
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+
+	// 提取所有磁力
+	var magnets []string
+	for _, item := range feed.Items {
+		for _, enclosure := range item.Enclosures {
+			magnets = append(magnets, enclosure.URL)
+		}
+	}
+	log.Printf("获取到磁力链接：%s", magnets)
+
+	client := resty.New()
+	client.SetHostURL(*qBittorrentApiUrl + "/api/v2")
+
+	// 设置 cookie jar
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+	client.SetCookieJar(jar)
+
+	// 登录
+	loginResp, err := client.R().SetQueryParams(map[string]string{
+		"username": *qBittorrentUsername,
+		"password": *qBittorrentPassword,
+	}).Get("/auth/login")
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+	log.Printf("登录状态：%s\n", string(loginResp.Body()))
+
+	// 新建任务
+	for _, magnet := range magnets {
+		addTorrentResp, err := client.R().SetQueryParams(map[string]string{
+			"urls": magnet,
+		}).Get("/torrents/add")
+		if err != nil {
+			log.Fatalf("%+v\n", err)
+		}
+		if strings.Contains(string(addTorrentResp.Body()), "Ok") {
+			log.Printf("添加磁力成功：%s\n", magnet)
+		} else {
+			log.Printf("添加磁力失败：%s\n", magnet)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/go-resty/resty/v2 v2.2.0
 	github.com/mmcdole/gofeed v1.0.0-beta2
 	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
