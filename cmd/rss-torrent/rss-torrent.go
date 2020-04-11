e334e0cf0814a463e8811831d5d7bf51cf3dcdd0
ziyuan <hanziyuan08@outlook.com>
2020-04-11 18:03:50 +0800

    second

--- cmd/rss-torrent/rss-torrent.go
+++ cmd/rss-torrent/rss-torrent.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"github.com/go-resty/resty/v2"
 	"github.com/mmcdole/gofeed"
+	"io/ioutil"
 	"log"
 	"net/http/cookiejar"
 	"strings"
 )
+
+const (
+	DatabaseFileName = "rss-middleware-database.json"
+)
+
+// RSS 中的磁力相关信息
+type RssMagnet struct {
+	Title  string // 磁力标题
+	GUID   string // 磁力唯一ID
+	Magnet string // 磁力链接
+	Read   bool   // 是否已读
+}
 
 func main() {
 	rssUrl := flag.String("rss", "", "rss url")
@@ -23,11 +37,21 @@ func main() {
 	}
 
 	// 提取所有磁力
-	var magnets []string
+	var rssMagnets []RssMagnet
 	for _, item := range feed.Items {
 		for _, enclosure := range item.Enclosures {
-			magnets = append(magnets, enclosure.URL)
+			rssMagnets = append(rssMagnets, RssMagnet{
+				Title:  item.Title,
+				GUID:   item.GUID,
+				Magnet: enclosure.URL,
+				Read:   false,
+			})
 		}
+	}
+
+	var magnets []string
+	for _, rssMagnet := range rssMagnets {
+		magnets = append(magnets, rssMagnet.Magnet)
 	}
 	log.Printf("获取到磁力链接：%s", magnets)
 
@@ -51,18 +75,59 @@ func main() {
 	}
 	log.Printf("登录状态：%s\n", string(loginResp.Body()))
 
+	// 查询所有已存储的 RSS 数据
+	savedRssMagnet := queryAllRssMagnet()
+	GUID2RssMagnet := map[string]RssMagnet{}
+	for _, rssMagnet := range savedRssMagnet {
+		GUID2RssMagnet[rssMagnet.GUID] = rssMagnet
+	}
+
+	// 待存储的 RSS 数据
+	var needSaveRssMagnet []RssMagnet
+
 	// 新建任务
-	for _, magnet := range magnets {
+	for _, magnet := range rssMagnets {
+		// 判断该 RSS 是否已读
+		if savedMagnet, ok := GUID2RssMagnet[magnet.GUID]; ok && savedMagnet.Read {
+			continue
+		}
+
 		addTorrentResp, err := client.R().SetQueryParams(map[string]string{
-			"urls": magnet,
+			"urls": magnet.Magnet,
 		}).Get("/torrents/add")
 		if err != nil {
 			log.Fatalf("%+v\n", err)
 		}
 		if strings.Contains(string(addTorrentResp.Body()), "Ok") {
-			log.Printf("添加磁力成功：%s\n", magnet)
+			log.Printf("添加磁力成功：%s\n", magnet.Magnet)
+			magnet.Read = true
 		} else {
-			log.Printf("添加磁力失败：%s\n", magnet)
+			log.Printf("添加磁力失败：%s\n", magnet.Magnet)
 		}
+		needSaveRssMagnet = append(needSaveRssMagnet, magnet)
 	}
+
+	needSaveRssMagnetBytes, err := json.Marshal(needSaveRssMagnet)
+	if err != nil {
+		log.Fatalf("序列化 RSS 数据失败: %+v\n", err)
+	}
+	err = ioutil.WriteFile(DatabaseFileName, needSaveRssMagnetBytes, 0777)
+	if err != nil {
+		log.Printf("保存 RSS 数据失败: %+v\n", err)
+	}
+}
+
+func queryAllRssMagnet() []RssMagnet {
+	dataBytes, err := ioutil.ReadFile(DatabaseFileName)
+	if err != nil {
+		log.Printf("查询既存数据失败：%+v", err)
+		return nil
+	}
+	var result []RssMagnet
+	err = json.Unmarshal(dataBytes, &result)
+	if err != nil {
+		log.Printf("序列化失败：%+v", err)
+		return nil
+	}
+	return result
 }
